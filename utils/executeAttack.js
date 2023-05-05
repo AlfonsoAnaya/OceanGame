805cc16ef56f5641216696beff5869bdf5eaf204
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2023-05-05 20:46:41 +0200

    final typescript util

--- utils/executeAttack.js
+++ utils/executeAttack.js
@@ -1,29 +1,34 @@
 export function executeAttack(tree, beach, ocean, wall, attackedSquare, squares, id, attackSelection, i, attackBtn) {
-    attackedSquare.classList.add('new-ocean')
+    attackedSquare.classList.add('new-ocean');
     if (attackedSquare.classList.contains('house')) {
-      attackedSquare.classList.remove('house');
-      attackedSquare.classList.add('tree');
-      attackedSquare.textContent = tree;
-    } else if (attackedSquare.classList.contains('tree')) {
-      attackedSquare.classList.remove('tree');
-      attackedSquare.classList.add('beach');
-      attackedSquare.textContent = beach;
-    } else if (attackedSquare.classList.contains('beach')) {
-      attackedSquare.classList.remove('beach');
-      attackedSquare.classList.add('ocean');
-      attackedSquare.textContent = ocean;
-    } else if (attackedSquare.classList.contains('wall')) {
-      attackedSquare.classList.remove('wall');
-      attackedSquare.classList.add('tree');
-      attackedSquare.textContent = tree;
-    } else if (attackedSquare.classList.contains('crane')) {
+        attackedSquare.classList.remove('house');
+        attackedSquare.classList.add('tree');
+        attackedSquare.textContent = tree;
+    }
+    else if (attackedSquare.classList.contains('tree')) {
+        attackedSquare.classList.remove('tree');
+        attackedSquare.classList.add('beach');
+        attackedSquare.textContent = beach;
+    }
+    else if (attackedSquare.classList.contains('beach')) {
+        attackedSquare.classList.remove('beach');
+        attackedSquare.classList.add('ocean');
+        attackedSquare.textContent = ocean;
+    }
+    else if (attackedSquare.classList.contains('wall')) {
+        attackedSquare.classList.remove('wall');
+        attackedSquare.classList.add('tree');
+        attackedSquare.textContent = tree;
+    }
+    else if (attackedSquare.classList.contains('crane')) {
         attackedSquare.classList.remove('crane');
         attackedSquare.classList.add('beach');
         attackedSquare.textContent = beach;
-      }
-    squares[id].classList.remove('ocean-attack');
-    console.log(attackSelection.length, i+1)
-    if (attackSelection.length === i+1) {
-      attackBtn.disabled = false
     }
-  }
+    squares[id].classList.remove('ocean-attack');
+    console.log(attackSelection.length, i + 1);
+    if (attackSelection.length === i + 1) {
+        attackBtn.disabled = false;
+    }
+}
+//# sourceMappingURL=executeAttack.js.map
---
+++ utils/executeAttack.ts
@@ -0,0 +1,39 @@
+export function executeAttack(
+  tree: string, 
+  beach: string, 
+  ocean: string, 
+  wall: string, 
+  attackedSquare: HTMLDivElement, 
+  squares: HTMLDivElement[], 
+  id: number, 
+  attackSelection: any, 
+  i: number, 
+  attackBtn: HTMLButtonElement) {
+    attackedSquare.classList.add('new-ocean')
+    if (attackedSquare.classList.contains('house')) {
+      attackedSquare.classList.remove('house');
+      attackedSquare.classList.add('tree');
+      attackedSquare.textContent = tree;
+    } else if (attackedSquare.classList.contains('tree')) {
+      attackedSquare.classList.remove('tree');
+      attackedSquare.classList.add('beach');
+      attackedSquare.textContent = beach;
+    } else if (attackedSquare.classList.contains('beach')) {
+      attackedSquare.classList.remove('beach');
+      attackedSquare.classList.add('ocean');
+      attackedSquare.textContent = ocean;
+    } else if (attackedSquare.classList.contains('wall')) {
+      attackedSquare.classList.remove('wall');
+      attackedSquare.classList.add('tree');
+      attackedSquare.textContent = tree;
+    } else if (attackedSquare.classList.contains('crane')) {
+        attackedSquare.classList.remove('crane');
+        attackedSquare.classList.add('beach');
+        attackedSquare.textContent = beach;
+      }
+    squares[id].classList.remove('ocean-attack');
+    console.log(attackSelection.length, i+1)
+    if (attackSelection.length === i+1) {
+      attackBtn.disabled = false
+    }
+  }
